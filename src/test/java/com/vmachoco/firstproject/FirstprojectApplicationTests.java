b75e1fc3f6deed2511852bf0363ff5315669d502
Machoco <victorino.machoco@gmail.com>
2023-05-25 17:42:08 +0200

    carregando todos arquivos

---
+++ src/test/java/com/vmachoco/firstproject/FirstprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vmachoco.firstproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
